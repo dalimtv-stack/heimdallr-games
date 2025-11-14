d5543ba36c7583be5f224c3e0b3e17660104e146
dalimtv-stack <dalim.tv@gmail.com>
2025-11-14 10:23:42 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['cheerio']
+  },
   images: {
-    domains: ['fitgirl-repacks.site', 'riotpixels.com', 'via.placeholder.com']
+    domains: ['fitgirl-repacks.site', 'riotpixels.com', 'via.placeholder.com', 'www.riotpixels.com']
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store' }
+        ]
+      }
+    ];
   }
 };
 
