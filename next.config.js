d83cce6f16def343aaff0c3498a586ae0cebd504
dalimtv-stack <dalim.tv@gmail.com>
2025-11-14 09:50:03 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['fitgirl-repacks.site', 'riotpixels.com', 'via.placeholder.com']
+  }
+};
+
+module.exports = nextConfig;
